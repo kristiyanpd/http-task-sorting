1b4c3f7ceb28a335cc9a315e69f6d74381167525
kristiyanpd <kristiyanpd02@gmail.com>
2023-05-01 12:00:33 +0300

    Add recursive topological task sorting

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -50,6 +50,65 @@ func (s *TaskService) SortTasks(tasks []model.Task) ([]*model.Task, error) {
 	return sortedTasks, nil
 }
 
+func (s *TaskService) SortTasksRecursively(tasks []model.Task) ([]*model.Task, error) {
+	visited := make(map[string]bool)
+	sorted := make([]*model.Task, 0, len(tasks))
+
+	for _, task := range tasks {
+		// Pick an unvisited node
+		if !visited[task.Name] {
+			err := s.topologicalSortHelper(task, tasks, visited, &sorted)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	return sorted, nil
+}
+
+func (s *TaskService) topologicalSortHelper(task model.Task, tasks []model.Task, visited map[string]bool, sorted *[]*model.Task) error {
+	if visited[task.Name] {
+		return errors.New("circular dependency found")
+	}
+	// Mark task as visited so circular checks can be done
+	visited[task.Name] = true
+
+	// Go through current task dependencies
+	for _, req := range task.Requires {
+		// Check if task is already sorted
+		if !s.containsTask(req, *sorted) {
+			reqTask := s.findTaskByName(req, tasks)
+			// DFS on required task
+			err := s.topologicalSortHelper(*reqTask, tasks, visited, sorted)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// Add task to sorted tasks so when iterating through other tasks' dependencies, we can skip it
+	*sorted = append(*sorted, &task)
+	return nil
+}
+
+func (s *TaskService) findTaskByName(taskName string, tasks []model.Task) *model.Task {
+	for _, task := range tasks {
+		if task.Name == taskName {
+			return &task
+		}
+	}
+	return &model.Task{}
+}
+
+func (s *TaskService) containsTask(taskName string, tasks []*model.Task) bool {
+	for _, task := range tasks {
+		if task.Name == taskName {
+			return true
+		}
+	}
+	return false
+}
+
 func (s *TaskService) GenerateBashScript(tasks []*model.Task, w io.Writer) error {
 	_, err := fmt.Fprintf(w, "#!/usr/bin/env bash\n")
 	if err != nil {
